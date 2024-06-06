d4246bb49cef04349c33ff484a4b40bf11da22c4
fedeval98 <fede.val9806@gmail.com>
2024-06-05 21:08:49 -0300

    añadiendo footer para web y mobile

--- src/main/resources/static/web/assets/js/home.js
+++ src/main/resources/static/web/assets/js/home.js
@@ -8,11 +8,19 @@ const options = {
       allcourses:[],
       allvideos:"",
       userID:"",
+      isWideScreen:false,
     } // finaliza return
   }, // finaliza data
   created() {
     this.loadData()
   }, //finaliza created
+  mounted(){
+    this.checkScreenSize()
+    window.addEventListener('resize', this.checkScreenSize)
+    },
+    beforeDestroy(){
+    window.removeEventListener('resize', this.checkScreenSize)
+    },
 
   methods: {
     loadData(){
@@ -25,7 +33,10 @@ const options = {
       console.log(this.userID)
     })
     .catch(error => {console.log(error)})
-  }//fin loadData
+  }, //fin loadData
+  checkScreenSize(){
+    this.isWideScreen = window.innerWidth >=1024
+    },
   }, //fin methods
 } //finaliza createApp
 
